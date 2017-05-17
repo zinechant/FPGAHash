197af25ccd3357d47a44bce3915653ece60bfae5
Chen Zou <chenzou@uchicago.edu>
2017-05-17 11:47:56 -0500

    64bit DataGen

---
+++ src/Common.h
@@ -0,0 +1,6 @@
+#define IntType long long
+const IntType N = 146800000;
+const int M = 4;
+const char * FileName[M] = {"../data/Linear.key", "../data/Random.key", 
+    "../data/Grid.key", "../data/RevGrid.key"};
+
--- src/DataGen.cpp
+++ src/DataGen.cpp
@@ -2,61 +2,73 @@
 #include <cstdlib>
 #include <string>
 
+#include "Common.h"
+
 using namespace std;
 
-void genLinear(const char * FilePath, int len){
+void genLinear(const char * FilePath, IntType len){
     FILE * Fo = fopen(FilePath, "w");
-    for (int i = 0; i < len; i++)
-        fprintf(Fo, "%8x\n", i+1);
+    for (IntType i = 0; i < len; i++)
+        fprintf(Fo, "%16x\n", i+1);
 }
-void genRandom(const char * FilePath, int len){
+void genRandom(const char * FilePath, IntType len){
     printf("RAND_MAX = %d\n", RAND_MAX);
     printf("RAND_MAX = %x\n", RAND_MAX);
     FILE * Fo = fopen(FilePath, "w");
-    for (int i = 0; i < len; i++)
-        fprintf(Fo, "%8x\n", rand());
+    for (int i = 0; i < len; i++){
+        IntType x = rand();
+        x = x * ((IntType)1+RAND_MAX) + rand()
+        fprintf(Fo, "%16x\n", x);
+    }
 
 }
-void genGrid(const char * FilePath, int len){
-    string s = "11111111";
+void genGrid(const char * FilePath, IntType len){
+    int K = sizeof(IntType);
+    unsigned char s[K];
+    for (int i = 0; i < K; i++){
+        s[i] = 1;
+    }
     FILE * Fo = fopen(FilePath, "w");
     for (; len; len--){
-        fprintf(Fo, "%s\n", s.c_str());
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s[i] == '9')
-                s[i] = 'A';
-            else if (s[i] == 'F')
-                s[i] = '1';
+        for (int i = 0; i < K; i++) {
+            fprintf(Fo, "%02x", s[i]);
+        }
+        fprintf(Fo, "\n");
+        for (int i = K-1; i >= 0; i--) {
+            if (s[i] == 14)
+                s[i] = 1;
             else s[i]++;
-
-            if (s[i] != '1')
+            if (s[i] != 1)
                 break;
         }
     }
 }
-void genRevGrid(const char * FilePath, int len){
-    string s = "11111111";
+void genRevGrid(const char * FilePath, IntType len){
+    int K = sizeof(IntType);
+    unsigned char s[K];
+    for (int i = 0; i < K; i++){
+        s[i] = 1;
+    }
     FILE * Fo = fopen(FilePath, "w");
     for (; len; len--){
-        fprintf(Fo, "%s\n", s.c_str());
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '9')
-                s[i] = 'A';
-            else if (s[i] == 'F')
-                s[i] = '1';
+        for (int i = 0; i < K; i++) {
+            fprintf(Fo, "%02x", s[i]);
+        }
+        fprintf(Fo, "\n");
+        for (int i = 0; i < K; i++) {
+            if (s[i] == 14)
+                s[i] = 1;
             else s[i]++;
-
-            if (s[i] != '1')
+            if (s[i] != 1)
                 break;
         }
     }
 }
 
 int main(){
-    int N = 1468000;
-    genLinear("../data/Linear.key", 1468000);
-    genRandom("../data/Random.key", 1468000);
-    genGrid("../data/Grid.key", 1468000);
-    genRevGrid("../data/RevGrid.key", 1468000);
+    genLinear(FileName[0], N);
+    genRandom(FileName[1], N);
+    genGrid(FileName[2], N);
+    genRevGrid(FileName[3], N);
     return 0;
 }
